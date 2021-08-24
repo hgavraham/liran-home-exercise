0f9170a79d9792bcf27942c6449c6ad7eba2f652
Ubuntu <ubuntu@ip-172-31-13-194.us-east-2.compute.internal>
2021-08-24 16:47:48 +0000

    add echo to update_nginx_configuration.sh

--- update_nginx_configuration.sh
+++ update_nginx_configuration.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+CONTID=$(docker ps | grep liran-nginx | awk '{print $1}')
+
 echo "====== update nginx conf file ======"
 echo ""
-echo "Plese enter the path of the new nginx.conf file:"
+echo "Please enter the path of the new nginx.conf file:"
 read -p "Full path: " USER_NGINX_CONF_PATH
+echo "copy $USER_NGINX_CONF_PATH to liran-nginx:/etc/nginx/conf/nginx.conf"
 docker cp $USER_NGINX_CONF_PATH liran-nginx:/etc/nginx/conf/nginx.conf
+echo "reload nginx container id: $CONTID"
 docker exec -it liran-nginx nginx -s reload
 echo "finished..."
 
